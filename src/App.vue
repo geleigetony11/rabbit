58745d2d8ed43d5065037463abc1156709a4a632
FuXin <1872755327@qq.com>
2023-08-11 18:25:12 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 //这个配置项仅作提示,不会时实际转化路径,vite.config.js中才是实际转化
-import HelloWorld from "@/views/HomeView.vue";
 </script>
 
 <template>
+  <!-- 一级路由组件出口 -->
+  <!-- 也可以写<router-view  /> -->
   <RouterView />
 </template>
-
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+//createRouter创建路由实例
+//createWebHistory创建history模式的路由
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  //path和component对应关系
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      component: Layout,
+      children: [
+        {
+          path: "", //这个是默认页
+          component: Home,
+        },
+        {
+          path: "category",
+          component: Category,
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/Login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
