6f5bcd8ad1fd261d5f695150ddc5fb4ce9216ef9
FuXin <1872755327@qq.com>
2023-09-04 15:28:16 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -35,6 +35,13 @@ export const useCartStore = defineStore(
       // cartList.value.filter((item) => skuId !== item.skuId);
     };
 
+    //单选功能
+    const singleCheck = (skuId, selected) => {
+      //通过skuId找到要修改的那一项，然后把他的selected修改为传过来的selected
+      const item = cartList.value.find((item) => item.skuId === skuId);
+      item.selected = selected;
+    };
+
     //计算属性
     //1.总数量-所有项的count之和
     const allCount = computed(() =>
@@ -51,6 +58,7 @@ export const useCartStore = defineStore(
       delCart,
       allCount,
       allPrice,
+      singleCheck,
     };
   },
   {
